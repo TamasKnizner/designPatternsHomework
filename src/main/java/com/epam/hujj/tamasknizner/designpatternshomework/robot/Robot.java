4b9d9327eb087844907949911be3c6173e1ffa2d
TamasKnizner <tamas_knizner@epam.com>
2016-10-02 19:53:14 +0200

    Robot dummy implementation

--- src/main/java/com/epam/hujj/tamasknizner/designpatternshomework/robot/Robot.java
+++ src/main/java/com/epam/hujj/tamasknizner/designpatternshomework/robot/Robot.java
@@ -1,5 +1,23 @@
 package com.epam.hujj.tamasknizner.designpatternshomework.robot;
 
+import java.util.LinkedList;
+
+import com.epam.hujj.tamasknizner.designpatternshomework.product.Product;
+
 public class Robot {
+
+	private LinkedList<Product> orders;
+
+	public Robot() {
+		this.orders = new LinkedList<>();
+	}
+
+	public void takeOrder(Product product) {
+		orders.add(product);
+	}
+
+	public Product serve() {
+		return orders.removeFirst();
+	}
 
 }
